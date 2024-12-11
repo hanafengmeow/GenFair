fcd4724eb5ecee0e5fa410c9350f520b3bed84d0
Yi Feng <feng.yi3@northeastern.edu>
2024-12-11 03:47:32 -0800

    fix states and ui interactions

--- app.py
+++ app.py
@@ -9,48 +9,24 @@ from genfair import QuestionLoader
 from genfair.llms import *
 from genfair.md_helper import get_section
 from genfair.st_elements import metrics_radar_chart
-from genfair.streamlit_helper import jump_to_tab
+from genfair.streamlit_helper import jump_to_tab, init_session_states
 
 
 QUESTION_TYPES = get_args(QuestionLoader.QuestionType.__value__)
 
 
+init_session_states({
+    'questions': defaultdict(list),
+    'responses': defaultdict(list),
+})
+         
 st.set_page_config(
     page_title="GenFair!",
     layout="wide",
 )
 
 st.title("GenFair: GENerative/GENder FAIRness Matters!")
-with st.sidebar:
-    model_type = st.selectbox(
-        'Please select a model type:', model_type_list := list(LLMMeta.registry),
-    )
-    llm_cls = LLMMeta.registry[model_type]
-    model_name = st.selectbox(
-        'Please select a model name to evaluate:', model_name_list := llm_cls.get_model_names()
-    )
-    api_key = st.text_input(
-        f'Enter {model_type} API key:',
-        type='password',
-    )
-    api_key_error = st.empty()
-    n_questions = st.slider(
-        label="Number of questions per dimension:",
-        min_value=1,
-        max_value=10,
-        value=3,
-        step=1
-    )
-    questionnaire_button = st.button('Generate Questionnaire')
-    response_button = st.button(
-        'Generate Response',
-        disabled='questions' not in st.session_state or not len(st.session_state.questions)
-    )
-    evaluate_button = st.button('Evaluate!', type='primary')
 
-if 'questions' not in st.session_state:
-    st.session_state.questions = defaultdict(list)
-            
 introduction, questionnaire, report, instructions = (tabs := st.tabs([
     "Introduction",
     "Questionnaire",
@@ -58,12 +34,61 @@ introduction, questionnaire, report, instructions = (tabs := st.tabs([
     "API Key Instructions",
 ]))
 
-if questionnaire_button:
-    jump_to_tab(tabs.index(questionnaire))
-    loader = QuestionLoader()
-    for question_type in QUESTION_TYPES:
-        st.session_state.questions[question_type] = loader.load_question_pairs(question_type, n=n_questions)
+with st.sidebar:
+    model_type = st.selectbox(
+        'Please choose candidate model type:', model_type_list := list(LLMMeta.registry),
+    )
+    llm_cls = LLMMeta.registry[model_type]
+    model_name = st.selectbox(
+        'Please choose candidate model name:', model_name_list := llm_cls.get_model_names()
+    )
+    candidate_api_key = st.text_input(
+        f'Enter {model_type} API key for the candidate model:',
+        type='password',
+    )
+    candidate_api_key_error = st.empty()
+    grader_api_key = st.text_input(
+        f'Enter Gemini API key for the grader model:',
+        type='password',
+    )
+    grader_api_key_error = st.empty()
+    n_questions = st.slider(
+        label="Number of questions per dimension:",
+        min_value=1,
+        max_value=10,
+        value=3,
+        step=1
+    )
     
+    st.divider()
+    
+    def questionnaire_button_callback():
+        loader = QuestionLoader()
+        for question_type in QUESTION_TYPES:
+            st.session_state.questions[question_type] = loader.load_question_pairs(question_type, n=n_questions)
+        st.session_state.responses.clear()
+        
+    questionnaire_button = st.button(
+        'Generate Questionnaire',
+        icon='🧐',
+        use_container_width=True,
+        on_click=questionnaire_button_callback,
+    )
+    
+    response_button = st.button(
+        'Begin Interview',
+        disabled=not any(st.session_state.questions.values()),
+        icon='💬',
+        use_container_width=True,
+    )
+    
+    evaluate_button = st.button(
+        '🔥 EVALUATE! 🔥',
+        type='primary',
+        disabled=not any(st.session_state.responses.values()),
+        use_container_width=True,
+    )
+
 with introduction:
     st.title("How it works?")
     st.video("https://youtu.be/99D4eS2F-p4")
@@ -73,12 +98,17 @@ with questionnaire:
     response_placesholders = defaultdict(list)
     for question_type in QUESTION_TYPES:
         st.title(question_type)
+        responses = st.session_state.responses[question_type] or defaultdict(lambda: (None, None))
         for i, (q1, q2) in enumerate(st.session_state.questions[question_type]):
             col1, col2 = st.columns(2)
-            with col1.expander(q1):
+            with col1.expander(q1, expanded=True):
                 ph1 = st.empty()
-            with col2.expander(q2):
+                if res := responses[i][0]:
+                    ph1.info(res)
+            with col2.expander(q2, expanded=True):
                 ph2 = st.empty()
+                if res := responses[i][1]:
+                    ph2.info(res)
             response_placesholders[question_type].append((ph1, ph2))
 
 with report:
@@ -159,29 +189,59 @@ with instructions:
             PLACEHOLDER
         """))
 
-async def generate_response(placeholder, topic):
-    model = llm_cls(model_name=model_name, api_key=api_key)
+async def generate_response(placeholder, question, question_type, question_index, pair_index):
+    model = llm_cls(model_name=model_name, api_key=candidate_api_key)
     streamed_text = ""
-    async for word in model.generate_async(topic):
+    async for word in model.generate_async(question):
         streamed_text = streamed_text + word
         placeholder.info(streamed_text)
+    return {
+        'question_type': question_type,
+        'question_index': question_index,
+        'pair_index': pair_index,
+        'question': question,
+        'response': streamed_text,
+    }
 
-async def main():
+async def interview():
     tasks = []
     for question_type in QUESTION_TYPES:
         for i, (q1, q2) in enumerate(st.session_state.questions[question_type]):
-            tasks.append(generate_response(response_placesholders[question_type][i][0], topic=q1))
-            tasks.append(generate_response(response_placesholders[question_type][i][1], topic=q2))
-        
-    await asyncio.gather(*tasks)
-
+            tasks.append(generate_response(
+                placeholder=response_placesholders[question_type][i][0],
+                question=q1,
+                question_type=question_type,
+                question_index=i,
+                pair_index=0,
+            ))
+            tasks.append(generate_response(
+                placeholder=response_placesholders[question_type][i][1],
+                question=q2,
+                question_type=question_type,
+                question_index=i,
+                pair_index=1,
+            ))
+    for question_type in QUESTION_TYPES:
+        st.session_state.responses[question_type] = [[None, None] for _ in range(n_questions)]
+    for response in await asyncio.gather(*tasks):
+        question_type = response['question_type']
+        question_index = response['question_index']
+        pair_index = response['pair_index']
+        st.session_state.responses[question_type][question_index][pair_index] = response['response']
+    st.rerun()
+    
+if questionnaire_button:
+    jump_to_tab(tabs.index(questionnaire))
+    
 if response_button:
-    if not api_key:
+    if not candidate_api_key:
         jump_to_tab(tabs.index(instructions))
-        api_key_error.error(f"Please provide {model_type} API key!")
+        candidate_api_key_error.error(f"Please provide {model_type} API key!")
     else:
         jump_to_tab(tabs.index(questionnaire))
-        asyncio.run(main())
-        
+        asyncio.run(interview())
+            
 if evaluate_button:
     jump_to_tab(tabs.index(report))
+    
+    
--- genfair/streamlit_helper.py
+++ genfair/streamlit_helper.py
@@ -1,6 +1,14 @@
+from typing import Any
 
+import streamlit as st
 from streamlit.components.v1 import html
 
+
+def init_session_states(defaults: dict[str, Any]):
+    for key, value in defaults.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
+            
 
 def jump_to_tab(tab_number: int):
     html(f"""
